a25c222e206573f9df8fb9f439ff8d6bba2e4e3c
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-10-10 21:52:51 -0700

    Finished modularizing the app

---
+++ server/config/routes.js
@@ -0,0 +1,14 @@
+var quotes = require('../controllers/quotes.js');
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
+        res.render('index');
+    });
+    // Add User Request 
+    app.post('/quotes', function(req, res) {
+        quotes.create(req, res);
+    });
+    app.get('/quotes', function(req, res) {
+        quotes.show(req, res);
+    });
+}
